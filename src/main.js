f4c4a8f3a4974d2ad6e951a2c349d15fa87795d5
vitaliy <vostapch@stud.hs-heilbronn.de>
2020-03-26 12:43:37 +0100

    Setup vuelidate plugin

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
+
 import App from './App.vue'
-import './registerServiceWorker'
+
 import router from './router'
 import store from './store'
+
+import './registerServiceWorker'
+
 import vuetify from './plugins/vuetify'
 import i18n from './plugins/vue-i18n'
+
 import './plugins/vue2-filters'
+import './plugins/vuelidate'
+
 
 Vue.config.productionTip = false
 
