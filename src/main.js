a948bea4fa2df6843cd9653306a226104ffdd196
vitaliy <vostapch@stud.hs-heilbronn.de>
2020-03-26 12:41:50 +0100

    Setup vue2-filters plugin

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import i18n from './plugins/vue-i18n'
+import './plugins/vue2-filters'
 
 Vue.config.productionTip = false
 
