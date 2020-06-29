96e8dd41a59aa1aac121ff5d27df152a2ae1b532
Vitaliy Ostapchuk <vitaliy.ostapchuk93@gmail.com>
2020-06-29 20:06:58 +0200

    Created projects container and items preview.

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+v-card(
+  class="pa-md-4 mx-auto"
+  outlined
+)
+  v-carousel(
+    height="400"
+    cycle
+    hide-delimiters
+    show-arrows-on-hover
+  )
+    v-carousel-item(
+      v-for="(picture, i) in projectPictures"
+      :key="i"
+    )
+      v-sheet(
+        color="white"
+        height="100%"
+      )
+        v-row(
+          class="fill-height"
+          align="center"
+          justify="center"
+        )
+          v-img(
+            :src="picture"
+            contain
+          )
+
+  v-card-title(
+    primary-title
+    class="headline primary--text"
+  ) {{projectName}}
+
+  v-card-text(
+    class="text--primary"
+  ) {{projectDescription}}
+
+  v-container
+    v-chip(
+      v-for="tag in projectTags"
+      :key="tag"
+      class="ma-1"
+      color="secondary"
+      outlined
+      small
+    ) {{tag}}
+
+  v-card-actions
+    v-btn(
+      color="deep-purple lighten-2"
+      text
+      @click="reserve"
+    ) Reserve
+</template>
+
+<script>
+export default {
+  name: 'ProjectItem',
+  props: {
+    projectPictures: {
+      type: Array,
+      required: true
+    },
+    projectName: {
+      type: String,
+      required: true
+    },
+    projectDescription: {
+      type: String,
+      required: true
+    },
+    projectTags: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    // ...
+  })
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/MyProjects.vue
+++ src/views/MyProjects.vue
@@ -1,12 +1,119 @@
 <template lang="pug">
+v-container
+  v-row
+    v-col(
+      v-for="project in projects"
+      :key="project.name"
+      :cols="projectColSize"
+    )
+      project-item(
+        :projectName="project.name",
+        :projectDescription="project.description"
+        :projectPictures="project.pictures"
+        :projectTags="project.tags"
+      )
+
 </template>
 
 <script>
+import ProjectItem from '../components/ProjectItem'
+
 export default {
   name: 'Projects',
+  components: {
+    ProjectItem
+  },
   data: () => ({
-    // ...
-  })
+    projects: [
+      {
+        name: 'Disease Pattern Miner',
+        description: 'Disease Pattern Miner is a free, open-source mining framework for interactively discovering sequential disease patterns in medical health record datasets ...',
+        pictures: [
+          require('../assets/projects/master/dashboard.png'),
+          require('../assets/projects/master/explorer.png'),
+          require('../assets/projects/master/results.png'),
+          require('../assets/projects/master/algo.png')
+        ],
+        tags: [
+          'Master Thesis',
+          'Java',
+          'Tomcat',
+          'Jenkins',
+          'Pattern Mining',
+          'Big Data'
+        ]
+      },
+      {
+        name: 'ARA',
+        description: 'Exposure therapy in Augmented Reality has proven its effectiveness in the treatment of small animal phobia. However, the conventional Augmented Reality Exposure Therapy confronts the user with visual stimulation only. The proposed concept called ARA aims to enhance the sense of realism by adding vibrotactile feedback to Augmented Reality Exposure Therapy ...',
+        pictures: [
+          require('../assets/projects/ara/concept.jpg'),
+          require('../assets/projects/ara/sleeve-and-holo.jpg'),
+          require('../assets/projects/ara/sleeve-and-pi.jpg'),
+          require('../assets/projects/ara/me-holo.png')
+        ],
+        tags: [
+          'Augmented Reality',
+          'Haptic Interfaces',
+          'Medical Computing',
+          'Small Animal Phobia',
+          'Unity',
+          'C#',
+          'Python',
+          'Raspberry Pi'
+        ]
+      },
+      {
+        name: 'Portfolio',
+        description: 'My personal portfolio built with Vue and hosted with Netlify ...',
+        pictures: [
+          require('../assets/projects/portfolio/home-desktop.png'),
+          require('../assets/projects/portfolio/home-mobile.png'),
+          require('../assets/projects/portfolio/timeline-desktop-1.png'),
+          require('../assets/projects/portfolio/menu-mobile.png'),
+          require('../assets/projects/portfolio/timeline-desktop-2.png')
+        ],
+        tags: [
+          'Vue',
+          'Netlify',
+          'YARN',
+          'JavaScript',
+          'Express',
+          'Vuetify'
+        ]
+      },
+      {
+        name: 'Navigation-assisted fibula reconstruction',
+        description: '',
+        pictures: [
+          require('../assets/projects/bachelor/computer-assisted-nav.png'),
+          require('../assets/projects/bachelor/nav-exp.png'),
+          require('../assets/projects/bachelor/app-1.png'),
+          require('../assets/projects/bachelor/app-2.png'),
+          require('../assets/projects/bachelor/mandibula-reconstruction.png')
+        ],
+        tags: [
+          'Bachelor Thesis',
+          'Medical Image Computing',
+          'C++',
+          'Qt',
+          'Signal Processing',
+          'Usability'
+        ]
+      }
+    ]
+  }),
+  computed: {
+    projectColSize: function () {
+      if (this.$vuetify.breakpoint.smAndDown) {
+        return 12
+      } else if (this.$vuetify.breakpoint.mdOnly) {
+        return 6
+      } else {
+        return 6
+      }
+    }
+  }
 }
 </script>
 
